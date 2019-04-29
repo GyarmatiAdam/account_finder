1e3541033cc68b46f032ecddbab8cafc940a5766
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-04-29 15:17:19 +0200

    blog and email
    blog and email also added to the list, what displayed

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function getProfile(){
 
         if(xhttp.readyState == 4 && xhttp.status == 200){
             var user = JSON.parse(xhttp.responseText);
-            var userName = document.getElementById('userName');
-            userName.innerHTML = user.name;
-            var avatar = document.getElementById('avatar');
-            avatar.innerHTML = `<img src="${user.avatar_url}" alt=""></img>`;
+            document.getElementById('userName').innerHTML = user.name;
+            document.getElementById('avatar').innerHTML = `<img src="${user.avatar_url}" alt=""></img>`;
+            document.getElementById('userBlog').innerHTML = "Website: " + `<a target="_blank" href="${user.blog}">Click here to wisit</a>`;
+            document.getElementById('userEmail').innerHTML = "Email: " + user.email;
+            document.getElementById('public_reps').innerHTML = `${user.public_repos} ` + `<a target="_blank" href="${user.repos_url}">Public Repositories</a>`;
+            document.getElementById('location').innerHTML = "Location: " + user.location;
         }
     }
 
