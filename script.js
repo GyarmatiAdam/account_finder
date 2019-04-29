5410fcb53ea6fe1539a67fe61044df99b84392ba
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-04-29 17:48:10 +0200

    ready functionality
    the app find github account by user login and displays its details.
    
    if there are no details, it sends messages

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function getProfile(){
-    var username = 'szabogergely07';
+function getProfile(e){
 
-    /* Create an AJAX request */
+/*prevent from submitting a form*/
+    e.preventDefault();
+
+    var username = document.getElementById('username').value;
+    if(!username || username == ''){
+        username = 'GyarmatiAdam'
+    }
+
+/* Create an AJAX request */
     var xhttp = new XMLHttpRequest();
-    /* Defines a function to be called when the readyState property changes */
+/* Defines a function to be called when the readyState property changes */
     xhttp.onreadystatechange = function(){
 
         if(xhttp.readyState == 4 && xhttp.status == 200){
@@ -11,15 +18,27 @@ function getProfile(){
             document.getElementById('userName').innerHTML = user.name;
             document.getElementById('avatar').innerHTML = `<img src="${user.avatar_url}" alt=""></img>`;
             document.getElementById('userBlog').innerHTML = "Website: " + `<a target="_blank" href="${user.blog}">Click here to wisit</a>`;
+            document.getElementById('bio').innerHTML = "About: <br>" + user.bio;
             document.getElementById('userEmail').innerHTML = "Email: " + user.email;
-            document.getElementById('public_reps').innerHTML = `${user.public_repos} ` + `<a target="_blank" href="${user.repos_url}">Public Repositories</a>`;
+            document.getElementById('public_reps').innerHTML = "Number of repositories: " + user.public_repos;
             document.getElementById('location').innerHTML = "Location: " + user.location;
+            document.getElementById('profil').innerHTML = `<a class="btn btn-danger" target="_blank" href="${user.html_url}">Profile</a>`;
+            
+/** if there is no such an info, than display those messages*/
+            if(user.email == undefined){
+                document.getElementById('userEmail').innerHTML = "Email: No email found";
+            }
+            if(user.blog == undefined){
+                document.getElementById('userBlog').innerHTML = "Website not available"
+            }
+        
         }
     }
 
     xhttp.open('GET', 'https://api.github.com/users/' + username, true);
-    /*	Sends the request to the server */
+/*	Sends the request to the server */
     xhttp.send();
 }
+/** submit the value from the input field */
+document.getElementById('user_input').addEventListener('submit', getProfile, false);
 
-getProfile();
